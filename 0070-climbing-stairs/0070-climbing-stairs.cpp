34b428f8f95ce6d19eec8129623171a867ee7971
Hayden030127 <129654648+HaydenHuan03@users.noreply.github.com>
2024-12-15 22:10:38 +0800

    Time: 0 ms (100%), Space: 8.6 MB (9.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n % 2 == 1){
-            
+        vector<int> memo(n+1, -1);
+        return helper(n, memo);
+    }
+
+    int helper(int n, vector<int>& memo){
+        if (n == 0 || n== 1){
+            return 1;
         }
+
+        if(memo[n] != -1){
+            return memo[n];
+        }
+
+        memo[n] = helper(n-1, memo) + helper(n-2, memo);
+        return memo[n];
     }
 };
