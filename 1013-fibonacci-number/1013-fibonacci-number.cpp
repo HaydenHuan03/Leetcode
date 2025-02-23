bb139a0b1fc0230a071e5df43a191998391141e8
Hayden030127 <129654648+HaydenHuan03@users.noreply.github.com>
2025-02-23 21:41:33 +0800

    Time: 2 ms (78.98%), Space: 7.8 MB (34.4%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0){
-            return 0;
+        if(n <= 1){
+            return n;
         }
 
-        if(n == 1 || n == 2){
-            return 1;
+        int prev1 = 0, prev2= 1, curr=0;
+
+        for(int i = 2; i <= n; i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
         }
 
-        return fib(n-1)+fib(n-2);
+        return curr;
     }
 };
