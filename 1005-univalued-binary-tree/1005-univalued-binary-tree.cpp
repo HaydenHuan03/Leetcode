327c638428059c006a1ef76f64980b6292b308ed
Hayden030127 <129654648+HaydenHuan03@users.noreply.github.com>
2025-03-15 00:34:42 +0800

    Time: 0 ms (100%), Space: 12.8 MB (8.52%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.cpp
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.cpp
@@ -11,16 +11,23 @@
  */
 class Solution {
 public:
-    bool isUnivalTree(TreeNode* root, int val){
-        if(!root) return true;
-
-        if(root->val != val) return false;
-
-        return isUnivalTree(root->left, val) && isUnivalTree(root->right, val);
-    }
-
     bool isUnivalTree(TreeNode* root) {
         if(!root) return true;
-        return isUnivalTree(root, root->val);
+
+        int val = root->val;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+
+            if(node->val != val) return false;
+
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
+
+        return true;
     }
 };
