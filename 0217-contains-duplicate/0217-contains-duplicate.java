5048ca22c82aa89419af8bc07d594b092cfc81c1
Hayden030127 <129654648+HaydenHuan03@users.noreply.github.com>
2025-05-22 23:07:50 +0800

    Time: 13 ms (68.12%), Space: 57.9 MB (75.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        // Solution 1:
+        // Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i] == nums[i + 1]){
-                return true;
-            }
+        // for(int i = 0; i < nums.length - 1; i++){
+        //     if(nums[i] == nums[i + 1]){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        //Solution 2:
+        Set<Integer> seen = new HashSet<>();
+
+        for(int num : nums){
+            if(seen.contains(num)) return true;
+            seen.add(num);
         }
 
         return false;
