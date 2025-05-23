51aecfda648c89b19db2ab4bed800eb2608ab6f0
Hayden030127 <129654648+HaydenHuan03@users.noreply.github.com>
2025-05-23 22:23:00 +0800

    Time: 4 ms (77.83%), Space: 45.2 MB (7.19%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //Solution 1
+        char[] string1 = s.toCharArray();
+        char[] string2 = t.toCharArray();
+
+        Arrays.sort(string1);
+        Arrays.sort(string2);
+
+        if(Arrays.equals(string1, string2)){
+            return true;
+        }
+
+        return false;
+    }
+}
