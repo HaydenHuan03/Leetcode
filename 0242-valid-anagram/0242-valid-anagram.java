05fec2ee5661ca545e9d2420d4fe18d41279a20d
Hayden030127 <129654648+HaydenHuan03@users.noreply.github.com>
2025-05-23 22:31:04 +0800

    Time: 6 ms (45.25%), Space: 43.2 MB (77.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         //Solution 1
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+
+        // char[] string1 = s.toCharArray();
+        // char[] string2 = t.toCharArray();
+
+        // Arrays.sort(string1);
+        // Arrays.sort(string2);
+
+        // if(Arrays.equals(string1, string2)){
+        //     return true;
+        // }
+
+        // return false;
+
+        //Solution 2
         if(s.length() != t.length()){
             return false;
         }
 
-        char[] string1 = s.toCharArray();
-        char[] string2 = t.toCharArray();
+        int[] count = new int[26];
 
-        Arrays.sort(string1);
-        Arrays.sort(string2);
-
-        if(Arrays.equals(string1, string2)){
-            return true;
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        return false;
+        for(int num : count){
+            if(num != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
